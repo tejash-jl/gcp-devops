51f9d4c87e30de46f3e35cf094849fe68312d241
Tejash <tjl1@deloitte.com>
2024-09-05 19:22:42 +0530

    Update timeout

--- deployments/scripts/keycloak-init.sh
+++ deployments/scripts/keycloak-init.sh
@@ -1,4 +1,4 @@
-kubectl wait pod --all --for=jsonpath='{.status.phase}'=Running  -n registry --timeout=60s
+kubectl wait pod --all --for=jsonpath='{.status.phase}'=Running  -n registry --timeout=200s
 echo -n "$1" | xargs -I '{}' sed -i -E 's@DOMAIN_VALUE@{}@' deployments/configs/keycloak-init-job.yaml
 
 kubectl apply -f deployments/configs/keycloak-init-job.yaml -n registry
